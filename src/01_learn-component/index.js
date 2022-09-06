517b3b812833456b998514b912691117b02c457d
lyh9322 <903037260@qq.com>
2022-09-06 16:09:35 +0800

    'learn-component'

--- src/01_learn-component/index.js
+++ src/01_learn-component/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 // import App from "./11_高阶组件的使用/02_props增强";
 // import App from "./11_高阶组件的使用/05_渲染-判断鉴权";
 // import App from "./11_高阶组件的使用/06_生命周期的劫持";
-import App from "./11_高阶组件的使用/07_生命周期的劫持-增强props";
+// import App from "./11_高阶组件的使用/07_生命周期的劫持-增强props";
+import App from "./12_组件内容补充/01_ref-函数组件"
 import "./css/NavBar.css";
 ReactDOM.render(<App />, document.getElementById("root"));
